8f035040fece9ab3718839a6cb2863f1a1eec3e1
lzoltan <lang_zoltan@yahoo.com>
2014-08-01 12:14:22 +0300

    Adding posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
   end
 
   def create
-  	@post = Post.new(params[:post])
+  	@post = Post.new(post_params)
+  	
   	if @post.save
   		redirect_to posts_path , :notice => "Your post was saved"
   	else
@@ -12,13 +13,29 @@ class PostsController < ApplicationController
   	end
   end
 
-  def new
-  	@post = Post.new
+  def update
+
   end
 
+  def destroy
 
+  end
+
+  def new
+  	@post = Post.new 
+  end
 
   def show
   	@post = Post.find(params[:id])
   end
+
+  private
+    # Using a private method to encapsulate the permissible parameters is just a good pattern
+    # since you'll be able to reuse the same permit list between create and update. Also, you
+    # can specialize this method with per-user checking of permissible attributes.
+    def post_params
+      params.required(:post).permit(:title, :content)
+    end
+
+
 end
