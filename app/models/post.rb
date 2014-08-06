d0b8fc83782d769436424b7e52bbc28cf24f2902
lzoltan <lang_zoltan@yahoo.com>
2014-08-06 17:28:50 +0300

    Added squeel queries to Post model, closes #4, closes #14

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,19 @@ class Post < ActiveRecord::Base
    validates :content, :presence => true
 
    belongs_to :user
+
+   def self.from_user(name)
+   		where{user_id.in(User.where{username.matches "#{name}" })}
+   end
+
+   def self.search(text)
+   		where{content.matches "#{text}"}
+   end
+
+   def self.from_time(ago) 
+   		where{created_at.gt "#{ago}"}
+   	end
+
+
+
 end
